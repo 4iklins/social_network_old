6035a7e1488969549f0d37ab2165da6284d7fe74
4iklins <52668068+4iklins@users.noreply.github.com>
2021-09-07 23:13:44 +0300

    add create post component

--- src/Content/Content.js
+++ src/Content/Content.js
@@ -1,9 +1,12 @@
 import './Content.css';
+import CreatePost from '../Create_post/Create_post';
 
 function Content() {
   return (
     <div className="Content">
-      <div className="main_content"></div>
+      <div className="main_content">
+        <CreatePost/>
+      </div>
       <div className="other_content"></div>
     </div>
   );
--- src/Create_post/Create_post.js
+++ src/Create_post/Create_post.js
@@ -1,10 +1,39 @@
 import './Create_post.css';
+import avatar from  '../img/avatar.jpg';
+import RoundBtn from '../Round_btn/Round_btn';
 
 
 function CreatePost() {
   return (
     <div className="Create_post">
-
+      <div className="create_post_text">
+        <div className="create_post_user_avatar">
+          <img src={avatar} alt="" />
+        </div>
+        <textarea name="post_text" id="" rows="5" placeholder="What's on your mind?"></textarea>
+      </div>
+      <div className="create_post_media">
+        <ul className="create_post_media_list">
+          <li className="create_post_media_item photo">
+            <a href="">
+              <div className="create_post_image photo"></div>
+            </a>
+          </li>
+          <li className="create_post_media_item video">
+            <a href="">
+              <div className="create_post_image video"></div>
+            </a>
+          </li>
+          <li className="create_post_media_item audio">
+            <a href="">
+              <div className="create_post_image audio"></div>
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div className="create_post_button">
+        <RoundBtn/>
+      </div>
     </div>
   );
 }
