bcc6690f0a169ae575d1b16ab3ce6e14a7e867f1
4iklins <4iklins@gmail.com>
2021-09-14 17:51:45 +0300

    add account and events components

---
+++ src/Events/Events.js
@@ -0,0 +1,34 @@
+import './Events.css';
+
+function Events() {
+  return (
+    <div className="events">
+      <p className="events_description">Events</p>
+      <ul className="events_list">
+        <li className="event_item">
+          <div className="event_date">FEB<span>22</span></div>
+          <div className="event_box">
+            <div className="event_head">Developer Programe</div>
+            <div className="event_descr">41 madison ave, floor 24 new work, NY 10010</div>
+          </div>
+        </li>
+        <li className="event_item">
+          <div className="event_date">MAR<span>13</span></div>
+          <div className="event_box">
+            <div className="event_head">B-day</div>
+            <div className="event_descr">46 pritickogo str, 3rd entrance, floor 7, BY 220082</div>
+          </div>
+        </li>
+        <li className="event_item">
+          <div className="event_date">APR<span>30</span></div>
+          <div className="event_box">
+            <div className="event_head">Aniversary Event</div>
+            <div className="event_descr">41 madison ave, floor 24 new work, NY 10010</div>
+          </div>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default Events
