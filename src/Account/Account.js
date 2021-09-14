249a873448bf1e3ff71ee11528fa9f4f5a8c0da9
4iklins <4iklins@gmail.com>
2021-09-14 17:52:21 +0300

    add accountjs

---
+++ src/Account/Account.js
@@ -0,0 +1,30 @@
+import './Account.css';
+import SmallBtn from '../Small_btn/Small_btn';
+
+function Account() {
+  return (
+    <div className="account">
+      <p className="account_description">Account</p>
+      <ul className="account_list">
+        <li className="account_item">
+          <a href="">
+            <div className="account_item_settings">
+              <SmallBtn />
+            </div>
+            <p>Settings</p>
+          </a>
+        </li>
+        <li className="account_item">
+          <a href="">
+            <div className="account_item_analytics">
+              <SmallBtn />
+            </div>
+            <p>Analytics</p>
+          </a>
+        </li>
+        </ul>
+    </div>
+  );
+}
+
+export default Account
