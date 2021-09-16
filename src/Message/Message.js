e8d3545ca4d5753a404b00da70e57ab18f23116b
4iklins <4iklins@gmail.com>
2021-09-16 17:51:21 +0300

    add messages components

---
+++ src/Message/Message.js
@@ -0,0 +1,11 @@
+import './Message.css';
+
+function Message() {
+  return (
+    <div className="message">
+      <p>dev by bor</p>
+    </div>
+  );
+}
+
+export default Message
--- src/Messages/Messages.js
+++ src/Messages/Messages.js
@@ -1,9 +1,26 @@
 import './Messages.css';
+import Message from '../Message/Message';
+import users from '../data/user.json';
+import {NavLink} from 'react-router-dom'
 
 function Messages() {
+  console.log(users)
   return (
     <div className="messages">
-      <p>тут будет чат</p>
+      <div className="dialogs">
+        <p className="dialogs_description">Dialogs</p>
+        <ul className="dialogs_list">
+          {users.map(item =>
+          <li className="dialogs_item">
+            <img src="" alt="" />
+            <NavLink to={`/messages/id${item.id}`} className="dialog_name">{item.name}</NavLink>
+          </li>
+          )}
+        </ul>
+      </div>
+      <div className="chat">
+
+      </div>
     </div>
   );
 }
