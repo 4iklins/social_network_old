10e60a7556a83a221d721b534802f216dd1604ea
4iklins <52668068+4iklins@users.noreply.github.com>
2021-09-20 02:45:37 +0300

    add users on chat

--- src/Messages/Messages.js
+++ src/Messages/Messages.js
@@ -1,10 +1,23 @@
 import './Messages.scss';
 import Message from '../Message/Message';
 import users from '../data/user.json';
-import {NavLink} from 'react-router-dom'
+import {NavLink} from 'react-router-dom';
+
+let Avatar = (item) => {
+  console.log(item.obj.avatar)
+  let name = item.obj.name;
+  let out = '';
+  name.split(' ').map((word) =>
+    out += word[0].toUpperCase()
+    )
+  if(item.obj.avatar !== ""){
+    return <img src={item.obj.avatar} alt="" />
+    }
+  else return <p className="user_without_avatar">{out}</p>
+};
+
 
 function Messages() {
-  console.log(users)
   return (
     <div className="messages">
       <div className="dialogs">
@@ -13,19 +26,26 @@ function Messages() {
         <ul className="dialogs_list">
           {users.map(item =>
           <li className="dialogs_item">
-            <img src="" alt="" />
+            <Avatar obj={item}/>
             <NavLink to={`/messages/id${item.id}`} className="dialog_name">{item.name}</NavLink>
           </li>
           )}
         </ul>
         </div>
-
       </div>
-      <div className="chat">
-
+      <div className="chat_wrapper">
+        <div className="chat">
+          <div className="chat_message_input">
+            <input type="text" />
+            <div className="chat_buttons">
+              <button className="chat_send"></button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
-}
+};
+
 
 export default Messages
