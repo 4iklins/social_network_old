40c9bd2240f5c9f1cc88a845a121b286e546a1f4
4iklins <4iklins@gmail.com>
2021-10-14 18:13:20 +0300

    add statejs

---
+++ src/data/state.js
@@ -0,0 +1,31 @@
+let state = {
+  dialogs:[  {
+    id: "1",
+    name: "Andrei Bakinovskiy",
+    avatar: ""
+  },
+  {
+    id: "2",
+    name: "Anthony Kiedis",
+    avatar: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/45/Red_Hot_Chili_Peppers_-_Rock_am_Ring_2016_-2016156230933_2016-06-04_%28cropped%29.jpg/800px-Red_Hot_Chili_Peppers_-_Rock_am_Ring_2016_-2016156230933_2016-06-04_%28cropped%29.jpg"
+  },
+  {
+    id: "3",
+    name: "Chad Smith",
+    "avatar": ""
+  },
+  {
+    id: "4",
+    "name": "Flea",
+    "avatar": ""
+  },
+  {
+    id: "5",
+    "name": "John Frusciante",
+    "avatar": ""
+  }],
+  messages:[],
+  posts:[]
+}
+
+export default state;
